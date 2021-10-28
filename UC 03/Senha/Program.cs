f0c6c0f2701e80071f6de3ad0847b9037fefe691
Williannca85 <87399058+Williannca85@users.noreply.github.com>
2021-10-28 19:47:29 -0300

    Adicionado UC-03 do curso

---
+++ UC 03/Senha/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+            int dia, mes, ano, idade;
+
+
+            Console.Clear();
+            Console.WriteLine("Digite o seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Olá " + nome);
+
+            //array ou vetor
+            String [] celulare = {"Xiaomi, LG, Sansumg"};
+
+            /*Console.WriteLine("Digite o seu dia de nascimento");
+            dia = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Digite o seu mês de nascimento");
+            mes = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Digite o seu ano de nascimento");
+            ano = Convert.ToInt16(Console.ReadLine());
+
+            idade = 2021 - ano;
+            Console.WriteLine("Olá " + nome + " você tem " + idade + " anos.");*/
+            
+            /* if (idade >= 18)
+            {
+                Console.WriteLine(idade + nome);
+            }
+            else
+            {
+                Console.WriteLine(nome + idade);
+            } */
+    
+    
+
+
+
+
+        }
+    }
+}
